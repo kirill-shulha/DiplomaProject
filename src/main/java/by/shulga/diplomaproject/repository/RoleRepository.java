3918366311e5bcb02b4bade0f036ffc0020fe280
kirill-shulha <strawberrydeejay@gmail.com>
2023-04-15 12:35:06 +0300

    Update project

--- src/main/java/by/shulga/diplomaproject/repository/RoleRepository.java
+++ src/main/java/by/shulga/diplomaproject/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package by.shulga.diplomaproject.repository;
 
 import by.shulga.diplomaproject.entity.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
 }
--- src/main/java/by/shulga/diplomaproject/repository/UserRepository.java
+++ src/main/java/by/shulga/diplomaproject/repository/UserRepository.java
@@ -2,9 +2,10 @@ package by.shulga.diplomaproject.repository;
 
 import by.shulga.diplomaproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLogin(String login);
